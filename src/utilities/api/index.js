b4e485a955488b8d2f8d2f60f491ad65d2f1bdc3
MINJE-98 <jmj012100@gmail.com>
2021-11-21 18:02:25 +0900

    TIL-110 Add index

--- src/utilities/api/index.js
+++ src/utilities/api/index.js
@@ -1,1 +1,5 @@
 export { fetchUserInfo, logout } from "./auth";
+export { createPost, updatePost, deletePost, viewDetailPost, viewSpecialUserPost, setLikePost, unSetLikePost } from "./posts"
+export { createComment, viewAllComment, createCommentToComment,viewComment, restoreComment, deleteComment } from "./comments"
+export { uploadImage } from "./upload"
+export { createUserBlogCustomization, updateUserBlogCustomization, deleteUserBlogCustomization, getUserBlogCustomization } from "./userBlogCustomization"
