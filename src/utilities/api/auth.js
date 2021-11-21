aac230d0975d6d47ea6d5ea1021cc7fd444a6ad2
MINJE-98 <jmj012100@gmail.com>
2021-11-21 16:25:56 +0900

    TIL-110 userinfo, logout 추가

---
+++ src/utilities/api/auth.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { server } from "./server";
+
+// 유저 정보를 가져옵니다.
+export const  fetchUserInfo = () =>{
+    return new Promise((resolve, reject) => {
+        axios.get(`${server}/auth/userinfo`, { withCredentials: true })
+        .then(({data}) =>{
+            resolve(data)
+        })
+        .catch(error =>{
+            reject(error)
+        })
+    })
+}
+
+// 로그아웃합니다.
+export const  logout = () =>{
+    return new Promise((resolve, reject) => {
+        axios.get(`${server}/auth/logout`, { withCredentials: true })
+        .then(({data}) =>{
+            resolve(data)
+        })
+        .catch(error =>{
+            reject(error)
+        })
+    })
+}
---
+++ src/utilities/api/index.js
@@ -0,0 +1,1 @@
+export { fetchUserInfo, logout } from "./auth";
