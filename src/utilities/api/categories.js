adf0ad38f063b492294eb81225022022677c0e42
MINJE-98 <jmj012100@gmail.com>
2021-11-21 18:01:59 +0900

    TIL-110 Add categories api

---
+++ src/utilities/api/categories.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import { server } from "../server"
+// 카테고리를 생성합니다.
+export const  createTag = (body) =>{
+    return new Promise((resolve, reject) => {
+        axios.post(`${server}/categories`, { 
+            data: {
+                categoryName: body.categoryName,
+            },
+            withCredentials: true })
+        .then(({data}) =>{
+            resolve(data)
+        })
+        .catch(error =>{
+            reject(error)
+        })
+    })
+}
