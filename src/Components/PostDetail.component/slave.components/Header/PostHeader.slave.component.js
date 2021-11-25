d4ff68fb079adcb281c3428c8f18d6e8c929d010
MINJE-98 <jmj012100@gmail.com>
2021-11-26 00:32:03 +0900

    TIL-128 포스트 디테일 컴포넌트 분리

---
+++ src/Components/PostDetail.component/slave.components/Header/PostHeader.slave.component.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  FaRegLaughSquint,
+  FaRegCalendarAlt,
+  FaRegEye,
+  FaBookmark,
+} from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { formatDistance, subDays } from "date-fns"
+
+
+export default function PostHeaderComponent({ title, createdAt, viewCounts, likes, categoryName }) {
+    return (
+        <div className="flex flex-col w-full max-w-5xl justify-start items-start ml-3">
+        <div>
+        {/* Title */}
+        <h1 className="text-5xl font-bold text-gray-800">
+            {title}
+        </h1>
+        </div>
+        {/* header Info */}
+        <div className="flex flex-row pt-3">
+        <div className="flex text-gray-600 mr-3">
+            <IconContext.Provider value={{ className: "mr-2 w-4 h-4" }}>
+            <FaRegCalendarAlt />
+            <span className="text-xs">{createdAt}</span>
+            </IconContext.Provider>
+        </div>
+        <div className="flex text-gray-600  mr-3">
+            <IconContext.Provider value={{ className: "mr-2 w-4 h-4" }}>
+            <FaRegEye />
+            <span className="text-xs">
+                {viewCounts} viewed
+            </span>
+            </IconContext.Provider>
+        </div>
+        <div className="flex text-gray-600  mr-3">
+            <IconContext.Provider value={{ className: "mr-2 w-4 h-4" }}>
+            <FaRegLaughSquint />
+            <span className="text-xs">{likes} likes</span>
+            </IconContext.Provider>
+        </div>
+        <div className="flex text-gray-600  mr-3">
+            <IconContext.Provider value={{ className: "mr-2 w-4 h-4" }}>
+            <FaBookmark />
+            <span className="text-xs">{categoryName}</span>
+            </IconContext.Provider>
+        </div>
+        </div>
+    </div>
+    )
+}
