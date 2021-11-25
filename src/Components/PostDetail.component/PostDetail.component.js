d7c4a0403c6a99296363de4129d3c3b66f85b407
argon1025 <argon1025@gmail.com>
2021-11-26 01:26:43 +0900

    Add PostDtailViewer

--- src/Components/PostDetail.component/PostDetail.component.js
+++ src/Components/PostDetail.component/PostDetail.component.js
@@ -6,66 +6,71 @@ import { useViewDetailPost } from "../../utilities/hooks";
 import PostHeaderComponent from "./slave.components/Header/PostHeader.slave.component";
 import PostLikeButtonComponent from "./slave.components/LikeButton/PostLikeButton.slave.component";
 import PostTagsComponent from "./slave.components/Tags/PostTags.slave.component";
-import PostCommentComponent from "../PostComment.component/PostComment.component"
+import PostCommentComponent from "../PostComment.component/PostComment.component";
+import { Tiptap } from "./slave.components/MarkdownViewer/MarkdownViewer.slave.component";
+
 export default function PostDetailComponent() {
-  const postData = useViewDetailPost(3)
-    return (
-      <div>
-        {/* Nav */}
-        <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-row max-w-5xl w-full">
-            {/* Logo */}
-            <div className="ml-5 mt-5 p-1 px-4 bg-black">
-              <h1 className="font-eng-sub-font-1 text-lg text-white">
-                .log
-              </h1>
-            </div>
-            {/* Login Button */}
-            <div className="ml-auto mr-5">
-              <button
-                type="button"
-                className="border text-black px-4 py-2 mt-4 transition duration-500 ease select-none hover:text-white hover:bg-black hover:border-black focus:outline-none focus:shadow-outline"
-              >
-                <div className="flex flex-row flex-nowrap align-middle justify-center items-center">
-                  <span className="text-sm">Login with Github</span>
-                  <IconContext.Provider value={{ className: "ml-2 w-7 h-7" }}>
-                    <DiGithubBadge />
-                  </IconContext.Provider>
-                </div>
-              </button>
-            </div>
+  const postData = useViewDetailPost(3);
+  return (
+    <div>
+      {/* Nav */}
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-row max-w-5xl w-full">
+          {/* Logo */}
+          <div className="ml-5 mt-5 p-1 px-4 bg-black">
+            <h1 className="font-eng-sub-font-1 text-lg text-white">.log</h1>
           </div>
-        </div>
-      {console.log(postData)}
-        {/* Banner */}
-        <PostBannerComponent thumbNailUrl={!postData ? <>fetching...</> : postData.thumbNailUrl} />
-
-        {/* Content */}
-        <div className="mt-10 flex flex-col w-full justify-center items-center">
-          {/* header */}
-        <PostHeaderComponent 
-        title={!postData ? <>fetching...</> : postData.title} 
-        createdAt={!postData ? <>fetching...</> : postData.createdAt} 
-        viewCounts={!postData ? <>fetching...</> : postData.viewCounts}
-        likes={!postData ? <>fetching...</> : postData.likes}
-        categoryName={!postData ? <>fetching...</> : postData.categoryName}/>
-
-          <hr className="border-gray-200 w-full mt-10" />
-
-          {/* Markdown Content */}
-          <div className="flex flex-col max-w-5xl w-full mt-10 ml-3">
-            <div>{!postData ? <>fetching...</>: postData.markDownContent}</div>
+          {/* Login Button */}
+          <div className="ml-auto mr-5">
+            <button
+              type="button"
+              className="border text-black px-4 py-2 mt-4 transition duration-500 ease select-none hover:text-white hover:bg-black hover:border-black focus:outline-none focus:shadow-outline"
+            >
+              <div className="flex flex-row flex-nowrap align-middle justify-center items-center">
+                <span className="text-sm">Login with Github</span>
+                <IconContext.Provider value={{ className: "ml-2 w-7 h-7" }}>
+                  <DiGithubBadge />
+                </IconContext.Provider>
+              </div>
+            </button>
           </div>
-
-          <hr className="border-gray-200 w-full my-10" />
-          {/* like Button */}
-          <PostLikeButtonComponent likes={!postData ? <>fetching...</> : postData.likes} />
-          {/* Tags */}
-          <PostTagsComponent tags={!postData ? [] : postData.TagData}/>
-
-          {/* Comments */}
-          <PostCommentComponent />
         </div>
       </div>
-    );
+      {console.log(postData)}
+      {/* Banner */}
+      <PostBannerComponent
+        thumbNailUrl={!postData ? <>fetching...</> : postData.thumbNailUrl}
+      />
+
+      {/* Content */}
+      <div className="mt-10 flex flex-col w-full justify-center items-center">
+        {/* header */}
+        <PostHeaderComponent
+          title={!postData ? <>fetching...</> : postData.title}
+          createdAt={!postData ? <>fetching...</> : postData.createdAt}
+          viewCounts={!postData ? <>fetching...</> : postData.viewCounts}
+          likes={!postData ? <>fetching...</> : postData.likes}
+          categoryName={!postData ? <>fetching...</> : postData.categoryName}
+        />
+
+        <hr className="border-gray-200 w-full mt-10" />
+
+        {/* Markdown Content */}
+        <div className="flex flex-col max-w-5xl w-full mt-10 ml-3">
+          <Tiptap contentData={!postData ? null : postData.markDownContent} />
+        </div>
+
+        <hr className="border-gray-200 w-full my-10" />
+        {/* like Button */}
+        <PostLikeButtonComponent
+          likes={!postData ? <>fetching...</> : postData.likes}
+        />
+        {/* Tags */}
+        <PostTagsComponent tags={!postData ? [] : postData.TagData} />
+
+        {/* Comments */}
+        <PostCommentComponent />
+      </div>
+    </div>
+  );
 }
---
+++ src/Components/PostDetail.component/slave.components/MarkdownViewer/MarkdownViewer.slave.component.js
@@ -0,0 +1,67 @@
+import React from "react";
+// src/Tiptap.jsx
+import { useEditor, EditorContent } from "@tiptap/react";
+import Document from "@tiptap/extension-document";
+import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import Typography from "@tiptap/extension-typography";
+import TextAlign from "@tiptap/extension-text-align";
+import Blockquote from "@tiptap/extension-blockquote";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Code from "@tiptap/extension-code";
+
+// load all highlight.js languages
+import lowlight from "lowlight";
+
+// string to json
+function stringToJson(contentData) {
+  try {
+    const markdownContentData = JSON.parse(contentData);
+
+    return markdownContentData;
+  } catch (error) {
+    return undefined;
+  }
+}
+
+export default function Tiptap(contentData) {
+  let editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        document: false,
+      }),
+      Document,
+      Highlight,
+      Typography,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Blockquote,
+      CodeBlockLowlight.configure({ lowlight }),
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Code,
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          "w-full h-full p-4 prose prose-sm max-w-none focus:outline-none overflow-y-auto",
+      },
+    },
+    autofocus: true,
+    editable: false,
+    content: stringToJson(contentData),
+  });
+
+  return (
+    <div>
+      <EditorContent className="w-full h-full" editor={editor} />
+    </div>
+  );
+}
+
+export { Tiptap };
