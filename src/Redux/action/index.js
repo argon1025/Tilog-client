d7075fb9cbf5d99f82575b5ed1b962726bc39f35
MINJE-98 <jmj012100@gmail.com>
2021-12-30 19:44:15 +0900

    TIL-185 철자 수정

--- src/Redux/action/index.js
+++ src/Redux/action/index.js
@@ -1,2 +1,2 @@
-export { GET_USER_USERINFO_FAILURE, GET_USER_USERINFO_SUCCESS } from "./atuhActions"
-export { getUserInfo } from './atuhActions'
+export { GET_USER_USERINFO_FAILURE, GET_USER_USERINFO_SUCCESS, GET_USER_USERINFO_SESSION_EXPIRED } from "./authActions"
+export { expiredUserSession, getUserInfo } from './authActions'
