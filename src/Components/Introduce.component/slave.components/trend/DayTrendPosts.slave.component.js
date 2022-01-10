0db114a396b2938e64f6320d71a2a237fbc863ee
argon1025 <argon1025@gmail.com>
2022-01-11 01:54:38 +0900

    Modify 메인디자인 수정, 스켈레톤 추가

--- src/Components/Introduce.component/slave.components/trend/DayTrendPosts.slave.component.js
+++ src/Components/Introduce.component/slave.components/trend/DayTrendPosts.slave.component.js
@@ -3,18 +3,32 @@ import { IconContext } from "react-icons";
 import { FaCheckDouble } from "react-icons/fa";
 import PostRank from "../postCard.component/postRank.slave.component";
 import { useDayTrendPosts } from "../../../../utilities/hooks";
+import PostRankSkeleton from "../postCard.component/postRankSkeleton.slave.component";
 
 export default function DayTrendPostsComponent({ searchScope }) {
-  const [trendList, error, errorMessage, statusCode, getNextPostList] = useDayTrendPosts(searchScope);
+  const [trendList, error, errorMessage, statusCode, getNextPostList] =
+    useDayTrendPosts(searchScope);
 
-  if (!statusCode) return <>스캘래톤</>;
+  if (!statusCode)
+    return (
+      <div className="w-full max-w-7xl flex flex-col md:flex-row md:flex-wrap md:gap-x-14 gap-y-12 justify-center items-center">
+        <PostRankSkeleton />
+        <PostRankSkeleton />
+        <PostRankSkeleton />
+        <PostRankSkeleton />
+        <PostRankSkeleton />
+        <PostRankSkeleton />
+      </div>
+    );
   if (error) return <>{errorMessage}</>;
   return (
     <div className="flex flex-col w-full justify-center items-center">
       {/* Card Area */}
       {/* Card */}
       <div className="w-full max-w-7xl flex flex-col md:flex-row md:flex-wrap md:gap-x-14 gap-y-12 justify-center items-center">
-        {trendList.map(post => <PostRank postData={post} />)}
+        {trendList.map((post) => (
+          <PostRank postData={post} />
+        ))}
         {/* Card End */}
       </div>
       {/* Post Load Button */}
@@ -24,7 +38,12 @@ export default function DayTrendPostsComponent({ searchScope }) {
         >
           <FaCheckDouble />
         </IconContext.Provider>
-        <p className="text-gray-400 text-xs" onClick={()=> getNextPostList(searchScope)} >Load More</p>
+        <p
+          className="text-gray-400 text-xs"
+          onClick={() => getNextPostList(searchScope)}
+        >
+          Load More
+        </p>
       </div>
     </div>
   );
