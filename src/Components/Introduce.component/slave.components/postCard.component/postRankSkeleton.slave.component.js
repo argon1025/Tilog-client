90258042772340792e16a6e64239bf55fdc11dea
argon1025 <argon1025@gmail.com>
2022-01-12 06:35:36 +0900

    Add 스켈레톤 다크모드

--- src/Components/Introduce.component/slave.components/postCard.component/postRankSkeleton.slave.component.js
+++ src/Components/Introduce.component/slave.components/postCard.component/postRankSkeleton.slave.component.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function PostRankSkeleton() {
   return (
-    <div className="animate-pulse h-96 w-full sm:w-80 transition duration-700 ease-in-out bg-white border border-gray-200 hover:shadow-xl">
-      <div className="flex justify-center items-center w-full h-36 bg-gray-200 text-gray-50 overflow-hidden">
+    <div className="animate-pulse h-96 w-full sm:w-80 transition duration-700 ease-in-out bg-white border border-gray-200 hover:shadow-xl dark:bg-gray-800 dark:border-gray-600">
+      <div className="flex justify-center items-center w-full h-36 bg-gray-200 text-gray-50 overflow-hidden dark:bg-gray-600">
         <span className="w-full break-words p-5 text-center"></span>
       </div>
 
@@ -12,20 +12,20 @@ export default function PostRankSkeleton() {
         {/* Category */}
         <div className="flex my-3">
           {/* content Icon */}
-          <div className="h-3 w-1/4 bg-gray-200 rounded"></div>
+          <div className="h-3 w-1/4 bg-gray-200 rounded dark:bg-gray-600"></div>
         </div>
         {/* Category End */}
         {/* Content */}
         <div className="flex flex-col justify-center h-full">
           {/* content title */}
-          <div className="h-4 w-full bg-gray-200 rounded mr-3"></div>
+          <div className="h-4 w-full bg-gray-200 rounded mr-3 dark:bg-gray-600"></div>
           <div className="flex flex-row items-center w-full mt-auto">
             <div className="flex mr-auto items-center">
               {/* Profile */}
-              <div className="h-5 w-5 rounded-full bg-gray-200 mr-1"></div>
-              <div className="h-4 w-10 bg-gray-200 rounded"></div>
+              <div className="h-5 w-5 rounded-full bg-gray-200 mr-1 dark:bg-gray-600"></div>
+              <div className="h-4 w-10 bg-gray-200 rounded dark:bg-gray-600"></div>
             </div>
-            <div className="h-4 w-16 bg-gray-200 rounded mr-3"></div>
+            <div className="h-4 w-16 bg-gray-200 rounded mr-3 dark:bg-gray-600"></div>
           </div>
         </div>
       </div>
--- src/Components/PostDetail.component/slave.components/Skeleton/ArticleSkeleton.slave.component.js
+++ src/Components/PostDetail.component/slave.components/Skeleton/ArticleSkeleton.slave.component.js
@@ -19,33 +19,33 @@ export default class ArticleSkeleton extends Component {
           <div className="w-full">
             {/* Title */}
             <div className="flex h-4 my-5">
-              <div className="w-4 h-4  bg-gray-200 rounded"></div>
-              <div className="w-10 bg-gray-200 m-1"></div>
+              <div className="w-4 h-4  bg-gray-200 dark:bg-gray-600 rounded"></div>
+              <div className="w-10 bg-gray-200 dark:bg-gray-600 m-1"></div>
             </div>
-            <div className="w-3/4 h-10  bg-gray-200 rounded"></div>
+            <div className="w-3/4 h-10  bg-gray-200 dark:bg-gray-600 rounded"></div>
           </div>
           {/* Info */}
           <div className="flex flex-row my-10 w-full">
-            <div className="w-10 h-4  bg-gray-200 rounded mr-3"></div>
-            <div className="w-10 h-4  bg-gray-200 rounded"></div>
+            <div className="w-10 h-4  bg-gray-200 dark:bg-gray-600 rounded mr-3"></div>
+            <div className="w-10 h-4  bg-gray-200 dark:bg-gray-600 rounded"></div>
           </div>
         </div>
         {/* title End */}
         <hr className="border-gray-200 w-full my-10 max-w-4xl mt-14" />
         {/* Content */}
         <div className="flex flex-col max-w-4xl w-full mt-10 ml-3">
-          <div className="w-full h-72 bg-gray-100 rounded"></div>
-          <div className="w-3/4 h-2 mt-3 bg-gray-100 rounded"></div>
-          <div className="w-3/5 h-2 mt-2 bg-gray-200 rounded"></div>
-          <div className="w-7/12 h-2 mt-2 bg-gray-100 rounded"></div>
-          <h1 className="text-7xl font-bold font-eng-sub-font-2 underline decoration-gray-100 text-gray-100">
+          <div className="w-full h-72 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <div className="w-3/4 h-2 mt-3 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <div className="w-3/5 h-2 mt-2 bg-gray-200 dark:bg-gray-600 rounded"></div>
+          <div className="w-7/12 h-2 mt-2 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <h1 className="text-7xl font-bold font-eng-sub-font-2 underline decoration-gray-100 text-gray-100 dark:text-blue-500 dark:decoration-gray-600 ">
             {title}
           </h1>
-          <div className="w-6/12 h-2 mt-2 bg-gray-200 rounded"></div>
-          <div className="w-4/6 h-2 mt-2 bg-gray-100 rounded"></div>
-          <div className="w-3/4 h-2 mt-20 bg-gray-100 rounded"></div>
-          <div className="w-2/4 h-2 mt-2 bg-gray-100 rounded"></div>
-          <div className="w-2/5 h-2 mt-2 bg-gray-100 rounded"></div>
+          <div className="w-6/12 h-2 mt-2 bg-gray-200 dark:bg-gray-600 rounded"></div>
+          <div className="w-4/6 h-2 mt-2 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <div className="w-3/4 h-2 mt-20 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <div className="w-2/4 h-2 mt-2 bg-gray-100 dark:bg-gray-700 rounded"></div>
+          <div className="w-2/5 h-2 mt-2 bg-gray-100 dark:bg-gray-700 rounded"></div>
         </div>
         {/* Content End */}
         <hr className="border-gray-200 w-full my-10 mt-14" />
@@ -60,8 +60,8 @@ export default class ArticleSkeleton extends Component {
           </div>
           {/* content */}
           <div className="w-full">
-            <div className="flex bg-gray-100 rounded-lg w-full h-28 mt-10">
-              <div className="px-4 bg-gray-100 w-full h-full rounded-l-lg text-base text-gray-500"></div>
+            <div className="flex bg-gray-100 dark:bg-gray-700 rounded-lg w-full h-28 mt-10">
+              <div className="px-4 bg-gray-100 dark:bg-gray-700 w-full h-full rounded-l-lg text-base text-gray-500"></div>
             </div>
           </div>
         </div>
