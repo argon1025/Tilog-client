4bdea0552a8e504a268dc73817868d6fa37a3678
MINJE-98 <jmj012100@gmail.com>
2022-03-19 17:07:05 +0900

    edit: Backend Server에서 비정상적으로 세션을 생성하는 행위로 인한 수정

--- src/utilities/api/TILog/comments.js
+++ src/utilities/api/TILog/comments.js
@@ -1,106 +1,108 @@
-import request from "./core"
-
+import request from './core';
 
 ///////////////// 인가가 필요한 요청 /////////////////
 
 // 포스트에 새로운 댓글을 생성합니다.
-const  createComment = (postsId, htmlContent) =>{
-    return request({
-        url: '/comments',
-        method: 'post',
-        data: {
-            postsId: postsId,
-            htmlContent: htmlContent
-        },
-        withCredentials: true
-    })
-}
-
+const createComment = (postsId, htmlContent) => {
+  return request({
+    url: '/comments',
+    method: 'post',
+    data: {
+      postsId: postsId,
+      htmlContent: htmlContent,
+    },
+    withCredentials: true,
+  });
+};
 
 // 댓글에 답글을 작성합니다.
-const  createReply = (commentId, postsId,  htmlContent) =>{
-    return request({
-        url: 'replies',
-        method: 'post',
-        data: {
-            id: commentId,
-            postsId: postsId,
-            htmlContent: htmlContent,
-            replyTo: 1,
-        },
-        withCredentials: true
-    })
-}
+const createReply = (commentId, postsId, htmlContent) => {
+  return request({
+    url: 'replies',
+    method: 'post',
+    data: {
+      id: commentId,
+      postsId: postsId,
+      htmlContent: htmlContent,
+      replyTo: 1,
+    },
+    withCredentials: true,
+  });
+};
 // 댓글을 수정 합니다.
-const  updateComment = (commentId, htmlContent) =>{
-    return request({
-        url: `/comments/${commentId}`,
-        method: 'patch',
-        data: {
-            htmlContent: htmlContent
-        },
-        withCredentials: true
-    })
-}
+const updateComment = (commentId, htmlContent) => {
+  return request({
+    url: `/comments/${commentId}`,
+    method: 'patch',
+    data: {
+      htmlContent: htmlContent,
+    },
+    withCredentials: true,
+  });
+};
 // 포스트의 모든 코멘트를 삭제합니다.
-const  deleteComment = (commentId) =>{
-    return request({
-        url: `/comments/${commentId}`,
-        method: 'delete',
-        withCredentials: true
-    })
-}
+const deleteComment = (commentId) => {
+  return request({
+    url: `/comments/${commentId}`,
+    method: 'delete',
+    withCredentials: true,
+  });
+};
 
 // 포스트의 모든 코멘트를 삭제합니다.
-const  restoreComment = (commentId) =>{
-    return request({
-        url: `/comments/restore/${commentId}`,
-        method: 'patch',
-        withCredentials: true
-    })
-}
+const restoreComment = (commentId) => {
+  return request({
+    url: `/comments/restore/${commentId}`,
+    method: 'patch',
+    withCredentials: true,
+  });
+};
 
 ///////////////// 인가가 필요없는 요청 /////////////////
 
 // 포스트의 모든 코멘트를 가져옵니다.
-const  getComments = (postId) =>{
-    return request({
-        url: `/posts/comments/${postId}`,
-        method: 'get',
-    })
-}
+const getComments = (postId) => {
+  return request({
+    url: `/posts/comments/${postId}`,
+    method: 'get',
+    withCredentials: true,
+  });
+};
 
 // 코멘트 모든 답글을 가져옵니다.
-const  getReplies = (commentid) =>{
-    return request({
-        url: `/comments/replies/${commentid}`,
-        method: 'get',
-    })
-}
+const getReplies = (commentid) => {
+  return request({
+    url: `/comments/replies/${commentid}`,
+    method: 'get',
+    withCredentials: true,
+  });
+};
 
 const getCommentsWriteUsers = (postID) => {
-    return request({
-        url: `/comments/writeusers/post/${postID}`,
-        method: 'get'
-    })
-} 
+  return request({
+    url: `/comments/writeusers/post/${postID}`,
+    method: 'get',
+    withCredentials: true,
+  });
+};
 
 // 특정 코멘트를 가져옵니다.
-const  getComment = (commentID) =>{
-    return request({
-        url: `comments/${commentID}`,
-        method: 'get'
-    })
-}
+const getComment = (commentID) => {
+  return request({
+    url: `comments/${commentID}`,
+    method: 'get',
+    withCredentials: true,
+  });
+};
 
 export {
-    createComment,
-    createReply,
-    deleteComment,
-    updateComment,
-    getComments,
-    getComment,
-    getReplies,
-    getCommentsWriteUsers,
-    restoreComment
-}
+  createComment,
+  createReply,
+  deleteComment,
+  updateComment,
+  getComments,
+  getComment,
+  getReplies,
+  getCommentsWriteUsers,
+  restoreComment,
+};
--- src/utilities/api/TILog/users.js
+++ src/utilities/api/TILog/users.js
@@ -1,11 +1,12 @@
-import request from "./core";
+import request from './core';
 
 // 유저 정보를 가져옵니다.
-const getUserInfoToUserName = (username)  => {
-    return request({
-        url: `/users/${username}`,
-        method: 'get'
-    })
-}
+const getUserInfoToUserName = (username) => {
+  return request({
+    url: `/users/${username}`,
+    method: 'get',
+    withCredentials: true,
+  });
+};
 
-export { getUserInfoToUserName }
+export { getUserInfoToUserName };
